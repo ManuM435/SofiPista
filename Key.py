0aa840e8bc81ddca8dcf8d73303f4b4bdd432ef0
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-28 22:21:20 -0300

    Code Finished

---
+++ Key.py
@@ -0,0 +1,64 @@
+# Run this code to get the key!
+# Then again... it might need a few performance improvements...
+# The main thing you gotta keep in mind is that the result is correct! It is exactly correct in the exact way the code is currently written
+# So make sure you don't alter that structure when making it faster, or you'll get the wrong key
+# But again, if you don't wanna take any chances by changing the code... you'll just have to be a bit patient while the key is calculated
+
+import numpy as np
+
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, keyS, t = np.linspace(0, 1, 1000000), np.linspace(1, 2, 1000000), np.linspace(2, 3, 1000000), np.linspace(3, 4, 1000000), np.linspace(4, 5, 1000000), np.linspace(5, 6, 1000000), np.linspace(6, 7, 1000000), np.linspace(7, 8, 1000000), np.linspace(0, 10, 11), np.linspace(9, 10, 1000000), np.linspace(10, 11, 1000000), np.linspace(11, 12, 1000000), np.linspace(12, 13, 1000000), np.linspace(13, 14, 1000000), [], []
+data = [a, b, c, d, e, f, g, h, j, k, l, m, n]
+print(len(data))
+
+def SuperMegaSecretDecryptationFunctionToObtainTheBaseForTheHiddenKey(Pr, data):
+    for it in range(len(data[2])):
+        for jt in range((len(data[11])**((it%2+1)))):
+            keyS.append(Pr(jt))
+    return keyS
+
+def Advanced_Processor_Function_To_Eliminate_Noise_And_Return_A_Base_Number_For_The_Key_To_Then_Be_Decrypted(data):
+    for x in range(len(data[5])):
+        t.extended[data[0][x], data[1][x], data[2][x], data[3][x], data[4][x], data[5][x], data[6][x], data[7][x], data[8][x], data[9][x], data[10][x], data[11][x], data[12][x]]
+        processingFactor += x / (1 + abs(a[x] / h[x] * j[x]))
+    return t
+
+def DerivingFunctionToFinallyReturnTheNumberUtilizingThePreviousFactorizationOfTheDecryptionOfTheProccessedData(result, xFactor):
+    dhfhfeuijdwbicxibeacyujxnisjcinjxiaxjincanibeufajcuibechneiubhauibuefhdnucsbhvdbkjdnjczij, result = 11, 300
+    yFactor = xFactor * 2 * 3 / 4 * 18 / 9
+    zFactor = yFactor ** (1/2)
+    
+    for i in range(1, 101):
+        result += (i % 10) * 2 - 1
+
+    intermediate = result * 2 + (xFactor * zFactor * (int(yFactor*11)%33))
+    intermediate = intermediate / 2
+    intermediate += sum(range(1, 11)) - sum(range(1, 6))
+    
+    for _ in range((result)**2 * (int(yFactor)%3) + 15):
+        intermediate += (-29)
+        intermediate -= 25
+
+    result = int(intermediate)
+
+    adjustment = (sum(range(1, 6)) - 5) * 2
+    result += adjustment
+
+    return result + dhfhfeuijdwbicxibeacyujxnisjcinjxiaxjincanibeufajcuibechneiubhauibuefhdnucsbhvdbkjdnjczij//5
+
+def Calculator_of_Factorization_Using_Prime_Numbers(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+key = DerivingFunctionToFinallyReturnTheNumberUtilizingThePreviousFactorizationOfTheDecryptionOfTheProccessedData(Calculator_of_Factorization_Using_Prime_Numbers((SuperMegaSecretDecryptationFunctionToObtainTheBaseForTheHiddenKey(Advanced_Processor_Function_To_Eliminate_Noise_And_Return_A_Base_Number_For_The_Key_To_Then_Be_Decrypted(data), data))[17])[0] + 478195213687/21, 12345)
+print(key)
+
+
